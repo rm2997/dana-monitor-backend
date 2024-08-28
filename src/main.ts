fa948605139e7e4c8386918d23bb0f9a947c44cd
rm2997 <rm2997@gmail.com>
2024-08-28 15:43:02 +0330

    mend

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: [`http://localhost:${process.env.APP_PORT}`],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   Logger.log(
     `Application is listening to port [${process.env.APP_PORT}]`,
     'DANA-API',
