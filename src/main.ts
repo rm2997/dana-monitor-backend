c760ea23fd498401ab1ac96e429056c1228af9e3
rm2997 <rm2997@gmail.com>
2024-09-08 17:08:44 +0330

    mend

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://dana-monitor-front/',
-      'http://10.2.10.44/',
-      'http://localhost',
-    ],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
     methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
-    allowedHeaders: ['content-type'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: true,
   });
   Logger.log(
     `Application is listening to port [${process.env.APP_PORT}]`,
