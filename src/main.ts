3ceb36f60be9344c215cf96a468d10ca96b9a3b8
rm2997 <rm2997@gmail.com>
2024-09-09 09:19:26 +0330

    mend

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'http://localhost:80',
+      'http://dana-monitor-backend:4000',
+    ],
     methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
